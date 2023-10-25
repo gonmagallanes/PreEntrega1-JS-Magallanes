6227273d70573107afe02f6cc5c455166dd69a5c
gonmagallanes <magallanesgonzalonicolas@gmail.com>
2023-10-25 18:51:44 -0300

    fin

---
+++ scripts/tercerPreentrega.js
@@ -0,0 +1,104 @@
+
+const agregarForm = document.querySelector("#agregar-form");
+const agregarInput = document.querySelector("#agregar-input");
+const agregar = document.querySelector("#agregar");
+
+let username = localStorage.getItem("usuario")
+agregarForm.addEventListener("submit", agregarItems);
+
+
+function agregarItems(e) {
+    e.preventDefault();
+
+    if (agregarInput.value != "") {
+        
+        texth2.innerText = `¡Bienvenido ${agregarInput.value}!`
+        localStorage.setItem("usuario", agregarInput.value)
+
+    } else {
+        alert("Input vacío!")
+    }
+
+    agregarInput.focus();
+    agregarForm.reset();
+}
+
+
+const tomate = document.querySelector("#tomate")
+const pollo = document.querySelector("#pollo")
+const pescado = document.querySelector("#pescado")
+const fideo = document.querySelector("#fideo")
+const agua = document.querySelector("#agua")
+const listado = document.querySelector("#listado")
+
+
+
+tomate.addEventListener("click", eliminarTomate)
+
+const listItems = document.querySelectorAll('#listado li');
+const toRemove = ['tomates'];
+
+function eliminarTomate() {
+    Array.from(listItems).forEach(listItem => {
+        if (toRemove.includes(listItem.textContent.toLowerCase())) {
+          listItem.parentNode.removeChild(listItem);
+        }
+      });
+      
+}
+
+pollo.addEventListener("click", eliminarPollo)
+
+const listItems2 = document.querySelectorAll('#listado li');
+const toRemove2 = ['pollo'];
+
+function eliminarPollo() {
+    Array.from(listItems2).forEach(listItem2 => {
+        if (toRemove2.includes(listItem2.textContent.toLowerCase())) {
+          listItem2.parentNode.removeChild(listItem2);
+        }
+      });
+      
+}
+
+pescado.addEventListener("click", eliminarPescado)
+
+const listItems3 = document.querySelectorAll('#listado li');
+const toRemove3 = ['pescado'];
+
+function eliminarPescado() {
+    Array.from(listItems3).forEach(listItem3 => {
+        if (toRemove3.includes(listItem3.textContent.toLowerCase())) {
+          listItem3.parentNode.removeChild(listItem3);
+        }
+      });
+      
+}
+
+fideo.addEventListener("click", eliminarFideo)
+
+const listItems4 = document.querySelectorAll('#listado li');
+const toRemove4 = ['fideos'];
+
+function eliminarFideo() {
+    Array.from(listItems4).forEach(listItem4 => {
+        if (toRemove4.includes(listItem4.textContent.toLowerCase())) {
+          listItem4.parentNode.removeChild(listItem4);
+        }
+      });
+      
+}
+
+agua.addEventListener("click", eliminarAgua)
+
+const listItems5 = document.querySelectorAll('#listado li');
+const toRemove5 = ['agua'];
+
+function eliminarAgua() {
+    Array.from(listItems5).forEach(listItem5 => {
+        if (toRemove5.includes(listItem5.textContent.toLowerCase())) {
+          listItem5.parentNode.removeChild(listItem5);
+        }
+      });
+      
+}
